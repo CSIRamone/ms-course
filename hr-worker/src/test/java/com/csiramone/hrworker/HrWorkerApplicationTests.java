c31f15eb97e67cc6e4a91e72d406093e6c05e911
Cidinei Inacio <cidineiinacio@MacBook-Pro-de-Cidinei.local>
2023-04-24 23:56:44 -0300

    Project hr-worker created

---
+++ hr-worker/src/test/java/com/csiramone/hrworker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csiramone.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
